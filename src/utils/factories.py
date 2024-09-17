81c4631a5bc005f0b08771eb7c0b8ccf5f4a0e39
NeiraJ <johnny.neira@gmail.com>
2024-09-17 11:18:58 +0200

    feat:  Extension des classes Creator et des fonctions utilitaires pour gérer les éditeurs
    Cette mise à jour apporte plusieurs améliorations et extensions au fichier factories.py :
    
    1. Importation de nouvelles classes :
       - EditeursDAO pour gérer la table des éditeurs
       - Editeur pour représenter les objets éditeur
    
    2. Ajout d'une nouvelle classe EditeursCreator :
       - Implémentation du patron Factory Method pour la création d'objets Editeur
    
    3. Ajout d'une nouvelle fonction create_all_editeurs_from_database() :
       - Permet de créer une liste d'objets Editeur à partir des données de la base de données
    
    Améliorations :
    - Cohérence avec les noms de classes mis à jour dans le reste du projet
    - Extension des capacités de création d'objets pour inclure les éditeurs
    - Maintien de la structure existante tout en ajoutant de nouvelles fonctionnalités
    
    Cette mise à jour améliore la flexibilité et l'extensibilité du système de création d'objets, permettant maintenant la gestion des livres, auteurs et éditeurs de manière cohérente.

--- src/utils/factories.py
+++ src/utils/factories.py
@@ -5,8 +5,10 @@
 from abc import ABC, abstractmethod
 
 from src.dao.auteurs_dao import AuteursDAO
+from src.dao.editeurs_dao import EditeursDAO
 from src.dao.livres_dao import LivresDAO
 from src.models.auteur import Auteur
+from src.models.editeur import Editeur
 from src.models.livre import Livre
 
 """
@@ -63,6 +65,32 @@ class AuteursCreator(Creator):
         auteur.id = information_source["id_auteur"]
 
         return auteur
+
+
+class EditeursCreator(Creator):
+
+    def factory_method(self, information_source: dict) -> Editeur:
+        """
+        Methode qui retourne un nouvel objet Editeur.
+        :param information_source:
+        """
+        editeur: Editeur = Editeur(
+            information_source["nom"],
+        )
+
+        editeur.id = information_source["id_editeur"]
+
+        return editeur
+
+
+def create_all_editeurs_from_database() -> list[Editeur]:
+    """
+    Creer tous les editeurs de la base de données.
+    :return: list[Editeurs]
+    """
+    editeurs = EditeursDAO()
+    results: list[dict] = editeurs.get_all()
+    return list(map(EditeursCreator().factory_method, results))
 
 
 def create_all_livres_from_database() -> list[Livre]:
