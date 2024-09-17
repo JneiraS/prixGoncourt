779111f4d4deaf0e96894410baa1a909c2cbe484
NeiraJ <johnny.neira@gmail.com>
2024-09-17 10:59:02 +0200

    feat: Ajout de la classe Auteur
    Cette mise à jour introduit une nouvelle classe `Auteur` représentant un auteur dans le système :
    
    - Attributs :
      - id (pour l'identifiant unique dans la base de données)
      - nom (nom de l'auteur)
      - biographie (biographie de l'auteur)
    
    Caractéristiques :
    - Utilisation de types Python pour améliorer la lisibilité et la maintenabilité
    - Initialisation des attributs via le constructeur (__init__)
    - Inclusion d'un champ 'id' pour la gestion dans la base de données
    
    Cette classe fournit une représentation structurée des auteurs dans le système, facilitant leur manipulation et leur stockage.

---
+++ src/models/auteur.py
@@ -0,0 +1,8 @@
+class Auteur:
+    """
+    Classe qui represente un auteur
+    """
+    def __init__(self, nom: str, biographie: str):
+        self.id = None
+        self.nom = nom
+        self.biographie = biographie
