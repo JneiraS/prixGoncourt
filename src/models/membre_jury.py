6695dbd4e16c19f9d180f25d57a612691f009c59
NeiraJ <johnny.neira@gmail.com>
2024-09-17 11:42:45 +0200

    feat: Ajout de la classe MembreJury
    Cette mise à jour introduit une nouvelle classe `MembreJury` représentant un membre du jury dans le système :
    
    - Attributs :
      - id (pour l'identifiant unique dans la base de données)
      - nom (nom du membre du jury)
      - role (rôle du membre du jury)
    
    Caractéristiques :
    - Utilisation de types Python pour améliorer la lisibilité et la maintenabilité
    - Initialisation des attributs via le constructeur (__init__)
    - Inclusion d'un champ 'id' pour la gestion dans la base de données
    
    Cette classe fournit une représentation structurée des membres du jury dans le système, facilitant leur manipulation et leur stockage.
    
    Améliorations :
    - Cohérence avec les bonnes pratiques de programmation orientée objet en Python
    - Préparation pour une éventuelle intégration avec le système de base de données

---
+++ src/models/membre_jury.py
@@ -0,0 +1,9 @@
+class MembreJury:
+    """
+    Classe qui represente un membre du jury
+    """
+
+    def __init__(self, nom: str, role: str):
+        self.id = None
+        self.nom = nom
+        self.role = role
