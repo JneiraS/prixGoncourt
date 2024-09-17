f609aaf11145d8f9f8d263e3d9d4d535a8c84de6
NeiraJ <johnny.neira@gmail.com>
2024-09-17 15:17:55 +0200

    feat: renforce la cohérence du projet en fournissant un moyen standardisé de référencer toutes les tables de la base de données.

---
+++ src/utils/tables_names.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class TableName(Enum):
+    """
+    Nom des tables de la base de données.
+    """
+
+    LIVRES = "livres"
+    AUTEURS = "auteurs"
+    EDITEURS = "editeurs"
+    MEMBER_JURY = "membre_jury"
+    PERSONNAGES = "personnages"
