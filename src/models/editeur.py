88258547beda3ec909f07be7b0309669c964d9b7
NeiraJ <johnny.neira@gmail.com>
2024-09-18 09:57:55 +0200

    feat: Modification de la classe Editeur pour inclure une liste statique d'éditeurs

--- src/models/editeur.py
+++ src/models/editeur.py
@@ -3,6 +3,10 @@ class Editeur:
     Classe qui represente un editeur
     """
 
+    editor_list: list = []
+
     def __init__(self, nom: str):
         self.id = None
         self.nom = nom
+
+        Editeur.editor_list.append(self)
