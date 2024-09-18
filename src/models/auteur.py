9698a8304bafaf9d85a1ef4635527186060df86a
NeiraJ <johnny.neira@gmail.com>
2024-09-18 09:57:33 +0200

    feat: Modification de la classe Auteur pour inclure une liste statique d'auteurs

--- src/models/auteur.py
+++ src/models/auteur.py
@@ -2,7 +2,12 @@ class Auteur:
     """
     Classe qui represente un auteur
     """
+
+    auteur_list: list = []
+
     def __init__(self, nom: str, biographie: str):
         self.id = None
         self.nom = nom
         self.biographie = biographie
+
+        Auteur.auteur_list.append(self)
