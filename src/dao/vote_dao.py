c4df0b76b276b7b96cf99360a4cde71207545561
NeiraJ <johnny.neira@gmail.com>
2024-09-19 15:59:44 +0200

    Ajouter méthodes pour obtenir les résultats des votes
    - Implémente get_voting_results_for pour récupérer les livres votés par ronde
    - Ajoute la fonction count_votes pour compter les votes d'un livre spécifique

--- src/dao/vote_dao.py
+++ src/dao/vote_dao.py
@@ -10,3 +10,25 @@ class VoteDAO(DatabaseConnectionManager):
     def __init__(self, table_name: str = TableName.VOTE.value):
         super().__init__(table_name)
         self.table_name = table_name
+
+    def get_voting_results_for(self, round_vote: int):
+        """
+        Renvoie les résultats de vote pour une tour de vote donnée.
+        :param round_vote: l'identifiant de la ronde de vote
+        :return: la liste des identifiants des livres qui ont  t  choisis
+        """
+        return self.query_database(
+            f"SELECT `id_livre` FROM `{self.table_name}` WHERE `id_selection` = {round_vote}"
+        )
+
+
+def count_votes(id_vivre: int):
+    """
+    Renvoie le nombre de votes d'un livre
+    :param id_vivre:
+    :return:
+    """
+    dao_tote = VoteDAO()
+    vote_dict = dao_tote.get_voting_results_for(2)
+
+    return vote_dict.count({"id_livre": id_vivre})
