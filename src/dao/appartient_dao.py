c5f751422e4fbbe978bb5abfe208304096e587e4
NeiraJ <johnny.neira@gmail.com>
2024-09-19 14:01:52 +0200

    Réorganiser AppartientDAO et déplacer fonctions associées
    - Supprimee l'import de query_database
    - Modifie la description de la classe
    - Déplace get_books_in_selection et insert_book_to_selection dans la classe

--- src/dao/appartient_dao.py
+++ src/dao/appartient_dao.py
@@ -1,12 +1,12 @@
 from src.dao.base_dao import DatabaseConnectionManager
 from src.models.livre import Livre
-from src.utils.database_utils import query_database
+
 from src.utils.tables_names import TableName
 
 
 class AppartientDAO(DatabaseConnectionManager):
     """
-    Classe qui s'occupe de la table livres.
+    Classe qui s'occupe de la table appartient.
     """
 
     def __init__(self, table_name: str = TableName.APPARTIENT.value):
@@ -17,40 +17,36 @@ class AppartientDAO(DatabaseConnectionManager):
         query: str = (
             f"SELECT * FROM `{TableName.APPARTIENT.value}` WHERE `id_selection` = {selection}"
         )
-        results: list[dict] = query_database(self, query)
+        results: list[dict] = self.query_database(query)
         return results
 
+    def get_books_in_selection(self, selection: int) -> list[Livre]:
+        """
+        Récupère tous les livres d'une sélection spécifique.
 
-def insert_book_to_selection(id_livre: int, id_selection: int):
-    """
-    Insère un livre dans une collection spécifique.
-    Cette méthode utilise l'objet DAO Appartient pour insérer une entrée
-    dans la table.
+        Cette méthode utilise l'objet DAO Appartient pour obtenir les livres
+        associés à une sélection donnée et les retourne sous forme de liste.
+        :param selection: l'identifiant de la selection
+        :return:
+        """
+        second_selection_list: list = []
+        results: list[dict] = self.get_selection(selection)
+        for result in results:
+            for book in Livre.book_list:
+                if result["id_livre"] == book.id:
+                    second_selection_list.append(book)
+        return second_selection_list
 
-    :param id_livre:
-    :param id_selection:
-    """
-    dao_appartient = AppartientDAO()
-    dao_appartient.create(
-        f"INSERT INTO `{TableName.APPARTIENT.value}` (`id_livre`, `id_selection`) VALUES "
-        f"('{id_livre}', '{id_selection}')"
-    )
+    def insert_book_to_selection(self, id_livre: int, id_selection: int):
+        """
+        Insère un livre dans une collection spécifique.
+        Cette méthode utilise l'objet DAO Appartient pour insérer une entrée
+        dans la table.
 
-
-def get_books_in_selection(selection: int) -> list[Livre]:
-    """
-    Récupère tous les livres d'une sélection spécifique.
-
-    Cette méthode utilise l'objet DAO Appartient pour obtenir les livres
-    associés à une sélection donnée et les retourne sous forme de liste.
-    :param selection: l'identifiant de la selection
-    :return:
-    """
-    second_selection_list: list = []
-    a_dao = AppartientDAO()
-    results: list[dict] = a_dao.get_selection(selection)
-    for result in results:
-        for book in Livre.book_list:
-            if result["id_livre"] == book.id:
-                second_selection_list.append(book)
-    return second_selection_list
+        :param id_livre:
+        :param id_selection:
+        """
+        self.create(
+            f"INSERT INTO `{TableName.APPARTIENT.value}` (`id_livre`, `id_selection`) VALUES "
+            f"('{id_livre}', '{id_selection}')"
+        )
