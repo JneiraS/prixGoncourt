c04846b2c6255011faf996df808c419a1fe6e369
NeiraJ <johnny.neira@gmail.com>
2024-09-19 14:19:07 +0200

    Ajouter méthode get_editeur_name_by_livre_id à la classe EditDAO

--- src/dao/edit_dao.py
+++ src/dao/edit_dao.py
@@ -1,12 +1,29 @@
 from src.dao.base_dao import DatabaseConnectionManager
+from src.models.editeur import Editeur
 from src.utils.tables_names import TableName
 
 
 class EditDAO(DatabaseConnectionManager):
     """
-    Classe qui s'occupe de la table livres.
+    Classe qui s'occupe de la table edit.
     """
 
     def __init__(self, table_name: str = TableName.EDIT.value):
         super().__init__(table_name)
         self.table_name = table_name
+
+    def get_editeur_name_by_livre_id(self, livre_id: int) -> str:
+        """
+        Récupère le nom de l'editeur d'un livre.
+        :param livre_id:
+        :return:
+        """
+        edit_table = self.get_all()
+
+        for edit in edit_table:
+            if edit["id_livre"] == livre_id:
+                for editeur in Editeur.editor_list:
+                    if edit["id_editeur"] == editeur.id:
+                        return editeur.nom
+
+        return ""
