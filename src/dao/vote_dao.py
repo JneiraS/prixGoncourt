791c65c727ece2389df07455a2c397c7c4ba3821
NeiraJ <johnny.neira@gmail.com>
2024-09-20 16:38:01 +0200

    feat: Amélioration de get_voting_results_for pour gérer le cas de la première sélection Ajout de has_voted_and_next_round_exists pour vérifier si un membre a déjà voté Implémentation de send_all_votes_to_db pour enregistrer les votes dans la base de données

--- src/dao/vote_dao.py
+++ src/dao/vote_dao.py
@@ -20,11 +20,18 @@ class VoteDAO(DatabaseConnectionManager):
         :param round_vote: l'identifiant de la ronde de vote
         :return: la liste des identifiants des livres qui ont  t  choisis
         """
-        return self.query_database(
-            f"SELECT id_livre, COUNT(*) AS nombre_de_votes FROM {self.table_name} WHERE "
-            f"id_selection={round_vote} GROUP BY id_livre ORDER BY nombre_de_votes DESC LIMIT"
-            f" {length_of_selection}"
-        )
+
+        if round_vote == 1:
+            return self.query_database(
+                f"SELECT * FROM {TableName.LIVRES.value} WHERE 1;"
+            )
+
+        else:
+            return self.query_database(
+                f"SELECT id_livre, COUNT(*) AS nombre_de_votes FROM {self.table_name} WHERE "
+                f"id_selection={round_vote} GROUP BY id_livre ORDER BY nombre_de_votes DESC LIMIT"
+                f" {length_of_selection}"
+            )
 
     def has_voted_and_next_round_exists(self, id_member: int, round_vote: int) -> bool:
         """
@@ -53,6 +60,17 @@ class VoteDAO(DatabaseConnectionManager):
                 == ()
             ):
                 return True
+
+    def send_all_votes_to_db(self, choices: list, id_member: int, vote_round: int):
+        coef: int = len(choices)
+
+        for choice in choices:
+            for _ in range(coef):
+                self.create(
+                    f"INSERT INTO `{self.table_name}` (`id_livre`, `id_membre`, `id_selection`) VALUES ("
+                    f"{choice},{id_member},{vote_round + 1})"
+                )
+            coef -= 1
 
 
 def count_votes(id_livre: int, round_vote: int):
