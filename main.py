12135db724690cf6eaee3b30341d812644a4c2d6
NeiraJ <johnny.neira@gmail.com>
2024-09-22 10:21:18 +0200

    refactor:Amélioration de l'initialisation de l'application
    - Ajout de la fonction clear_screen() pour nettoyer l'écran avant l'affichage du menu
    - Simplification de la structure du script principal

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from src.display.display import clear_screen
 from src.display.menus import menu_commun
 from src.utils.factories import initialize_database_in_threads
 
 
 def main():
-
     # Initialise la base de données en utilisant des threads.
     initialize_database_in_threads()
     # Affiche le menu commun
+    clear_screen()
+
     menu_commun()
 
 
